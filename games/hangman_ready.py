87622888498597a237a224ed19fcc4a50474b76a
doyoung930 <70666642+doyoung930@users.noreply.github.com>
2022-05-23 19:30:34 +0900

    Update hangman_ready.py

--- games/hangman_ready.py
+++ games/hangman_ready.py
@@ -1,3 +1,4 @@
+from ast import Delete
 import math
 from tkinter import * # Import tkinter
 from random import randint
@@ -6,12 +7,14 @@ class Hangman:
     hiddenWord, guessWord, nMissedLetters = [], [], set()   # 파일에서 찾은 단어, 맞추고 있는 단어, 틀린 단어 set
     nCorrectChar, nMissChar = 0, 0                          # 맞춘 개수, 틀린 개수
     finished = 0                                            # 0 = 아직 안 끝남, 1 = 맞춤, 2 = 틀림
+    
 
     def __init__(self, words):
         self.hiddenWord = words
         self.guessWord = ['*' for _ in range(len(self.hiddenWord))]        # word의 길이 만큼 * 문자 채우기, draw에서는 guessWord를 보일거임.
         print(self.hiddenWord)
-        #print(self.guessWord)
+          
+        print(self.guessWord)
         print(type(self.nMissedLetters))
         self.draw()
 
@@ -25,14 +28,18 @@ class Hangman:
         canvas.create_line(60, 200, 60, 20, tags = "hangman")  # Draw the pole
         canvas.create_line(60, 20, 160, 20, tags = "hangman") # Draw the hanger
         
+        canvas.create_text(300,150,text=self.guessWord, tags = "hangman")
+        if(len(self.nMissedLetters) > 0 ):
+            canvas.create_text(200,130,text = self.nMissedLetters , tags = "hangman")
+
         radius = 20 # 반지름
         
         if(self.nMissChar >= 1):                                    # 줄
-            canvas.create_line(160, 20, 160, 40, tags = "hangman")  # Draw the hanger
+            줄 = canvas.create_line(160, 20, 160, 40, tags = "hangman")  # Draw the hanger
 
         if(self.nMissChar >= 2):                                    # 머리
         # Draw the circle
-            canvas.create_oval(140, 40, 180, 80, tags = "hangman")  # Draw the hanger
+            머리 =canvas.create_oval(140, 40, 180, 80, tags = "hangman")  # Draw the hanger
 
         if(self.nMissChar >= 3):                                    # 왼쪽 팔
         # Draw the left arm (중심(160,60)에서 45도 움직인 지점의 x좌표는 cos로, y좌표는 sin으로 얻기)
@@ -41,7 +48,7 @@ class Hangman:
             x2 = 160 - (radius+60) * math.cos(math.radians(45))
             y2 = 60 + (radius+60) * math.sin(math.radians(45))
 
-            canvas.create_line(x1, y1, x2, y2, tags = "hangman")
+            왼쪽팔 = canvas.create_line(x1, y1, x2, y2, tags = "hangman")
         
         if(self.nMissChar >= 4):                                    # 오른팔
             x3 = 160 + radius * math.cos(math.radians(45))
@@ -49,7 +56,7 @@ class Hangman:
             x4 = 160 + (radius+60) * math.cos(math.radians(45))
             y4 = 60 + (radius+60) * math.sin(math.radians(45))
 
-            canvas.create_line(x3, y3, x4, y4, tags = "hangman")
+            오른팔 = canvas.create_line(x3, y3, x4, y4, tags = "hangman")
 
         if(self.nMissChar >= 5):                                    # 몸통
             x5 = 160
@@ -57,7 +64,7 @@ class Hangman:
             x6 = 160
             y6 = 60 + radius + 60
 
-            canvas.create_line(x5, y5, x6, y6, tags = "hangman")
+            몸통 = canvas.create_line(x5, y5, x6, y6, tags = "hangman")
 
         if(self.nMissChar >= 6):                                    # 왼쪽 다리
             x7 = 160
@@ -65,7 +72,7 @@ class Hangman:
             x8 = 160 - (radius+60) * math.cos(math.radians(45))
             y8 = 60 + (radius+60) * math.sin(math.radians(45)) + 60
 
-            canvas.create_line(x7, y7, x8, y8, tags = "hangman")
+            왼쪽다리 = canvas.create_line(x7, y7, x8, y8, tags = "hangman")
 
         if(self.nMissChar >= 7):                                    # 오른 다리
             x9 = 160
@@ -73,10 +80,22 @@ class Hangman:
             x10 = 160 + (radius+60) * math.cos(math.radians(45))
             y10 = 60 + (radius+60) * math.sin(math.radians(45)) + 60
 
-            canvas.create_line(x9, y9, x10, y10, tags = "hangman")
+            오른다리 = canvas.create_line(x9, y9, x10, y10, tags = "hangman")
+        if(self.nMissChar >= 8):
+            canvas.create_text(100,300,text = "게임이 종료되었습니다. 다시 시작하려면 enter를 누르시오." , tags = "hangman")
+            self.setWord(self)
     
     def setWord(self):                                              # 새로운 단어를 선택하고 게임 (재)시작
-        pass
+        
+        window.event_delete(self.줄, self.머리, self.몸통, self.왼쪽팔, self.오른팔, self.왼쪽다리, self.오른다리)
+       
+        self.hiddenWord, self.guessWord, self.nMissedLetters = [], [], set()   # 파일에서 찾은 단어, 맞추고 있는 단어, 틀린 단어 set
+        self.nCorrectChar, self.nMissChar = 0, 0                          # 맞춘 개수, 틀린 개수
+        self.finished = 0                                            # 0 = 아직 안 끝남, 1 = 맞춤, 2 =  틀림
+        random_number = randint(0, len(words) - 1)                          # 읽어온 단어들 전체 중 하나를 랜덤하게 선택
+        hangman = Hangman(words[random_number])                             # 랜덤하게 선택한 단어로 클래스 생성
+        window.update()
+        self.draw()
 
     def guess(self, letter):                                        # 사용자가 입력한 글자를 반영
         self.corret = False                                         # 함수에 들어왔을 때, 한 번이라도 맞췄는가?의 대한 불 함수를 False로 초기화.
