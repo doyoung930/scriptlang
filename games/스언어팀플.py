100a8640b83a1afcc7c8c04a4932a9486240577e
이 도 <dleh99@naver.com>
2022-06-12 21:23:53 +0900

    오른쪽 상세정보 표시 완성
    야호

--- games/스언어팀플.py
+++ games/스언어팀플.py
@@ -133,10 +133,10 @@ def event_for_listbox(event):
                 if name == data[1]:
                     _text1 = "시설명 : " + getStr(item.find('FACLT_NM').text)
                     _text2 ='시군명 : ' + getStr(item.find('SIGUN_NM').text)
-                    _text3 ="도로명 주소 : " + getStr(item.find('REFINE_ROADNM_ADDR'))
-                    _text4 = "연락처 : " + getStr(item.find('CONTCT_NO'))
-                    _text5 = "홈페이지 주소 : " + getStr(item.find('HMPG_ADDR'))
-                    _text6 = "면적(건축) : " + getStr(item.find('BUILD_AR'))
+                    _text3 ="도로명 주소 : " + getStr(item.find('REFINE_ROADNM_ADDR').text)
+                    _text4 = "연락처 : " + getStr(item.find('CONTCT_NO').text)
+                    _text5 = "홈페이지 주소 : " + getStr(item.find('HMPG_ADDR').text)
+                    _text6 = "면적(건축) : " + getStr(item.find('BUILD_AR').text) + "m^2"
                     break
         
         # 선택 안 했다면 모든 xml에서 다 찾아
@@ -145,64 +145,60 @@ def event_for_listbox(event):
             for item in elements:
                 name = item.find('FACLT_NM').text
                 if name == data[1]:
-                    print(data[1])
                     find_in_dontselect = True
                     _text1 = "시설명 : " + getStr(item.find('FACLT_NM').text)
                     _text2 ='시군명 : ' + getStr(item.find('SIGUN_NM').text)
-                    _text3 ="도로명 주소 : " + getStr(item.find('REFINE_ROADNM_ADDR'))
-                    _text4 = "연락처 : " + getStr(item.find('CONTCT_NO'))
-                    _text5 = "홈페이지 주소 : " + getStr(item.find('HMPG_ADDR'))
-                    _text6 = "면적(건축) : " + getStr(item.find('BUILD_AR'))
+                    _text3 ="도로명 주소 : " + getStr(item.find('REFINE_ROADNM_ADDR').text)
+                    _text4 = "연락처 : " + getStr(item.find('CONTCT_NO').text)
+                    _text5 = "홈페이지 주소 : " + getStr(item.find('HMPG_ADDR').text)
+                    _text6 = "면적(건축) : " + getStr(item.find('BUILD_AR').text) + "m^2"
                     break
             if(not find_in_dontselect):
                 elements = get_xml_soccer()
                 for item in elements:
                     name = item.find('FACLT_NM').text
                     if name == data[1]:
-                        print(data[1])
                         find_in_dontselect = True
                         _text1 = "시설명 : " + getStr(item.find('FACLT_NM').text)
                         _text2 ='시군명 : ' + getStr(item.find('SIGUN_NM').text)
-                        _text3 ="도로명 주소 : " + getStr(item.find('REFINE_ROADNM_ADDR'))
-                        _text4 = "연락처 : " + getStr(item.find('CONTCT_NO'))
-                        _text5 = "홈페이지 주소 : " + getStr(item.find('HMPG_ADDR'))
-                        _text6 = "면적(건축) : " + getStr(item.find('BUILD_AR'))
+                        _text3 ="도로명 주소 : " + getStr(item.find('REFINE_ROADNM_ADDR').text)
+                        _text4 = "연락처 : " + getStr(item.find('CONTCT_NO').text)
+                        _text5 = "홈페이지 주소 : " + getStr(item.find('HMPG_ADDR').text)
+                        _text6 = "면적(건축) : " + getStr(item.find('BUILD_AR').text) + "m^2"
                         break
             if(not find_in_dontselect):
                 elements = get_xml_swim()
                 for item in elements:
                     name = item.find('FACLT_NM').text
                     if name == data[1]:
-                        print(data[1])
                         find_in_dontselect = True
                         _text1 = "시설명 : " + getStr(item.find('FACLT_NM').text)
                         _text2 ='시군명 : ' + getStr(item.find('SIGUN_NM').text)
-                        _text3 ="도로명 주소 : " + getStr(item.find('REFINE_ROADNM_ADDR'))
-                        _text4 = "연락처 : " + getStr(item.find('CONTCT_NO'))
-                        _text5 = "홈페이지 주소 : " + getStr(item.find('HMPG_ADDR'))
-                        _text6 = "면적(건축) : " + getStr(item.find('BUILD_AR'))
+                        _text3 ="도로명 주소 : " + getStr(item.find('REFINE_ROADNM_ADDR').text)
+                        _text4 = "연락처 : " + getStr(item.find('CONTCT_NO').text)
+                        _text5 = "홈페이지 주소 : " + getStr(item.find('HMPG_ADDR').text)
+                        _text6 = "면적(건축) : " + getStr(item.find('BUILD_AR').text) + "m^2"
                         break
             if(not find_in_dontselect):
                 elements = get_xml_inside()
                 for item in elements:
                     name = item.find('FACLT_NM').text
                     if name == data[1]:
-                        print(data[1])
                         find_in_dontselect = True
                         _text1 = "시설명 : " + getStr(item.find('FACLT_NM').text)
                         _text2 ='시군명 : ' + getStr(item.find('SIGUN_NM').text)
-                        _text3 ="도로명 주소 : " + getStr(item.find('REFINE_ROADNM_ADDR'))
-                        _text4 = "연락처 : " + getStr(item.find('CONTCT_NO'))
-                        _text5 = "홈페이지 주소 : " + getStr(item.find('HMPG_ADDR'))
-                        _text6 = "면적(건축) : " + getStr(item.find('BUILD_AR'))
+                        _text3 ="도로명 주소 : " + getStr(item.find('REFINE_ROADNM_ADDR').text)
+                        _text4 = "연락처 : " + getStr(item.find('CONTCT_NO').text)
+                        _text5 = "홈페이지 주소 : " + getStr(item.find('HMPG_ADDR').text)
+                        _text6 = "면적(건축) : " + getStr(item.find('BUILD_AR').text) + "m^2"
                         break
     
-    info_listbox.insert(1, _text1)
-    info_listbox.insert(1, _text2)
-    info_listbox.insert(1, _text3)
-    info_listbox.insert(1, _text4)
-    info_listbox.insert(1, _text5)
-    info_listbox.insert(1, _text6)
+        info_listbox.insert(1, _text1)
+        info_listbox.insert(1, _text2)
+        info_listbox.insert(1, _text3)
+        info_listbox.insert(1, _text4)
+        info_listbox.insert(1, _text5)
+        info_listbox.insert(1, _text6)
 
 # 농구장 xml
 def get_xml_basket():
